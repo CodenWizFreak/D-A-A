c5ed78141725f2203f5c8e778263994ed9a01fa0
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-04-21 02:27:18 +0530

    Create dijkstra.c

---
+++ day_5/dijkstra.c
@@ -0,0 +1,13 @@
+/*
+WAP to implement Dijkstra’s Algorithm for Single Source Shortest path problem. Your program should use priority queue implementation. Check your program on the following directed graph. The graph should be implemented by adjacency list.
+
+   s    t    x    y    z
+s  0   10   INF   5   INF
+t  INF  0    1    2   INF
+x  INF INF   0   INF   4
+y  INF  3    9    0    2
+z  7   INF   6   INF   0
+
+
+
+*/
