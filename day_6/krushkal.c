e8ad075d956bd37267d56ae3905b6393824a0ed7
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-04-29 23:51:14 +0530

    Update krushkal.c

--- day_6/krushkal.c
+++ day_6/krushkal.c
@@ -2,4 +2,18 @@
 WAP to find the MST of a given graph using Kruskal’s algorithm. Check your
 program on a graph whose length matrix is given below. Your program should
 use UNION & FIND operations on Disjoint data structure.
+
+   a b c d e f g h i
+
+a  0 4 ∞ ∞ ∞ ∞ ∞ 8 ∞
+b  4 0 8 ∞ ∞ ∞ ∞ 11 ∞
+c  ∞ 8 0 7 ∞ 4 ∞ ∞ 2
+d  ∞ ∞ 7 0 9 14 ∞ ∞ ∞
+e  ∞ ∞ ∞ 9 0 10 ∞ ∞ ∞
+f  ∞ ∞ 4 14 10 0 2 ∞ ∞
+g  ∞ ∞ ∞ ∞ ∞ 2 0 1 6
+h  8 11 ∞ ∞ ∞ ∞ 1 0 7
+i  ∞ ∞ 2 ∞ ∞ ∞ 6 7 0
+
+
 */
