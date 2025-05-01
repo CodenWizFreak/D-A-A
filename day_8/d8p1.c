eb45258eefae02626294987aab30fe156c7ec945
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-01 21:41:19 +0530

    Create d8p1.c

---
+++ day_8/d8p1.c
@@ -0,0 +1,6 @@
+/*
+WAP to implement Knuth-Morris-Pratt (KMP) algorithm for pattern matching.
+Check your program for the following Text & Pattern:
+T: b a c b a b a b a c a b a b a c a
+P: a b a b a c a
+*/
