1700fa859afd319ecf632d516450cb269b67be57
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-01 21:41:49 +0530

    Update d8p1.c

--- day_8/d8p1.c
+++ day_8/d8p1.c
@@ -4,3 +4,12 @@ Check your program for the following Text & Pattern:
 T: b a c b a b a b a c a b a b a c a
 P: a b a b a c a
 */
+
+#include <stdio.h>
+#include <string.h>
+
+// Function to compute LPS array
+void computeLPSArray(char* pat, int M, int* lps) {
+    int len = 0;
+    lps[0] = 0; // lps[0] is always 0
+    int i = 1;
