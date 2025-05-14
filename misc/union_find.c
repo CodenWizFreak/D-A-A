c3d9ef2e9e037292c596a1d7ee15aa6cbb123068
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-15 00:03:15 +0530

    Update union_find.c

--- misc/union_find.c
+++ misc/union_find.c
@@ -7,3 +7,12 @@
 int parent[MAX], rank[MAX];
 char vertices[MAX][3];
 int vertexCount = 0;
+
+int findVertexIndex(char* v) {
+    for (int i = 0; i < vertexCount; i++) {
+        if (strcmp(vertices[i], v) == 0)
+            return i;
+    }
+    strcpy(vertices[vertexCount], v);
+    return vertexCount++;
+}
