a125e127ade23c0115e4e0bd438315d662f06ee8
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-17 10:14:09 +0530

    Update heapsort.c

--- misc/heapsort.c
+++ misc/heapsort.c
@@ -17,3 +17,25 @@ int bestCase[MAX], worstCase[MAX];
 void swap(int *a, int *b) {
     int t = *a; *a = *b; *b = t;
 }
+
+void copyArray(int *dest, int *src, int len) {
+    for (int i = 0; i < len; i++) dest[i] = src[i];
+}
+
+void printArray(int *a, int len) {
+    for (int i = 0; i < len; i++) printf("%d ", a[i]);
+    printf("\n");
+}
+
+// Heapify
+void heapify(int arr[], int size, int i) {
+    int largest = i, l = 2 * i + 1, r = 2 * i + 2;
+
+    if (l < size && arr[l] > arr[largest]) largest = l;
+    if (r < size && arr[r] > arr[largest]) largest = r;
+
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, size, largest);
+    }
+}
