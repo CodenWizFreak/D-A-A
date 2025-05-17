78710f0db03e6a78a7b5352e5f757ce13febcd7c
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-17 09:33:18 +0530

    Update queue_using_stack.c

--- misc/queue_using_stack.c
+++ misc/queue_using_stack.c
@@ -11,3 +11,19 @@ typedef struct Stack {
 void initStack(Stack* s) {
     s->top = -1;
 }
+
+int isEmpty(Stack* s) {
+    return s->top == -1;
+}
+
+int isFull(Stack* s) {
+    return s->top == MAX - 1;
+}
+
+void push(Stack* s, int val) {
+    if (isFull(s)) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    s->data[++(s->top)] = val;
+}
