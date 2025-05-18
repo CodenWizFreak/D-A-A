a27b7089b3e8ef7ff3539bf2b015a61faa668ec4
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-18 09:23:05 +0530

    Create prims_minheap.c

---
+++ misc/prims_minheap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define MAXV 100          /* max distinct vertex labels */
+#define MAXNAME 32
+
+/* ---------- vertex/edge structures ---------- */
+typedef struct Edge {
+    int to;
+    int w;
+    struct Edge *next;
+} Edge;
+
+typedef struct {
+    char name[MAXNAME];
+    Edge *adj;
+} Vertex;
