23e5b1b33c5975cfd00a6d6de7ec30ef1215c9e2
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-20 00:00:12 +0530

    Update fractional_knapsack.c

--- misc/fractional_knapsack.c
+++ misc/fractional_knapsack.c
@@ -11,3 +11,16 @@ void swap(struct Item *a, struct Item *b) {
     *a = *b;
     *b = temp;
 }
+
+// Sort by profit descending, and for equal profits by ratio descending
+void sortItems(struct Item items[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (items[j].profit < items[j + 1].profit ||
+               (items[j].profit == items[j + 1].profit &&
+                items[j].ratio < items[j + 1].ratio)) {
+                swap(&items[j], &items[j + 1]);
+            }
+        }
+    }
+}
