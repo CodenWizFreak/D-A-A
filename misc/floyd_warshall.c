e6fd5576e0fff9643559b1e8e0f5a36e534651be
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-23 00:45:59 +0530

    Update floyd_warshall.c

--- misc/floyd_warshall.c
+++ misc/floyd_warshall.c
@@ -8,3 +8,13 @@
 
 int nodeCount = 0;
 char nodes[MAX]; // Maps index to node name
+
+// Get index of node name, add if not exists
+int getNodeIndex(char c) {
+    for (int i = 0; i < nodeCount; i++) {
+        if (nodes[i] == c)
+            return i;
+    }
+    nodes[nodeCount] = c;
+    return nodeCount++;
+}
