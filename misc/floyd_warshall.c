4313affee44826135bdd8b5b63ab6afb13527f48
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-23 00:45:33 +0530

    Create floyd_warshall.c

---
+++ misc/floyd_warshall.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+#define MAX 26
+#define INF 99999
+
+int nodeCount = 0;
+char nodes[MAX]; // Maps index to node name
