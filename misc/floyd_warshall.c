c997db323792324bd5c41d89eaf0c39469ab2cfb
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-23 00:46:25 +0530

    Update floyd_warshall.c

--- misc/floyd_warshall.c
+++ misc/floyd_warshall.c
@@ -18,3 +18,21 @@ int getNodeIndex(char c) {
     nodes[nodeCount] = c;
     return nodeCount++;
 }
+
+void printSolution(int dist[MAX][MAX]) {
+    printf("Shortest distances between every pair of vertices:\n\n   ");
+    for (int i = 0; i < nodeCount; i++)
+        printf(" %c ", nodes[i]);
+    printf("\n");
+
+    for (int i = 0; i < nodeCount; i++) {
+        printf(" %c ", nodes[i]);
+        for (int j = 0; j < nodeCount; j++) {
+            if (dist[i][j] == INF)
+                printf(" ∞ ");
+            else
+                printf("%2d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
