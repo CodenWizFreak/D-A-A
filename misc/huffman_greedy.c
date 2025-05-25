4b9d6681f0261156abbc230bc7e6a925d53ca475
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-25 17:35:54 +0530

    Create huffman_greedy.c

---
+++ misc/huffman_greedy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* A Huffman tree node */
+typedef struct MinHeapNode {
+    unsigned char data;            /* Stored character */
+    unsigned freq;                 /* Frequency of the character */
+    struct MinHeapNode *left, *right; /* Left and right child */
+} MinHeapNode;
+
+/* Min‑heap: collection of heap (or Huffman tree) nodes */
+typedef struct MinHeap {
+    unsigned size;      /* Current number of nodes */
+    unsigned capacity;  /* Heap capacity */
+    MinHeapNode **array; /* Array of node pointers */
+} MinHeap;
