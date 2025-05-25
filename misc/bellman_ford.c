1aee64b171c4566fcd9ef1d5ac1b8d76af4b1485
Ananyo Dasgupta <dasguptaananyo28@gmail.com>
2025-05-25 17:54:38 +0530

    Update bellman_ford.c

--- misc/bellman_ford.c
+++ misc/bellman_ford.c
@@ -17,3 +17,21 @@ int vertexIndex(char c) {
 char vertexChar(int i) {
     return 'a' + i;
 }
+
+void bellmanFord(Edge edges[], int E, int V, int src, int dist[]) {
+    // Initialize distances
+    for (int i = 0; i < V; i++)
+        dist[i] = INF;
+    dist[src] = 0;
+
+    // Relax edges V-1 times
+    for (int i = 1; i <= V - 1; i++) {
+        for (int j = 0; j < E; j++) {
+            int u = edges[j].from;
+            int v = edges[j].to;
+            int w = edges[j].weight;
+            if (dist[u] != INF && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
